a50deda5d6d62d869eff23a3a17223970e330eb5
Indah <indahwsn@gmail.com>
2020-09-20 10:49:59 +0000

    Add new file

---
+++ script.js
@@ -0,0 +1,19 @@
+const number1 = 103;
+const number2 = 72;
+const number3 = 189;
+
+const getMax = (a, b, c) => {
+  let max = a;
+  
+  if (b > max) {
+    max = b;
+  }
+  if (c > max) {
+    max = c;
+  }
+  
+  return max;
+};
+
+const max = getMax(number1, number2, number3);
+console.log(`Nilai maksimum adalah ${max}`);
